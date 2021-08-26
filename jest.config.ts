95a0b644ca78d614b42f0ab399145151e3e9fac6
dry <dengry.diy@gmail.com>
2021-08-26 20:03:42 +0800

    feat: add jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,23 +6,24 @@ const config: Config.InitialOptions = {
     '^.+\\.ts$': 'ts-jest'
   },
   testEnvironmentOptions: {
-    'jest-playwright': {}
+    'jest-playwright': {
+      browsers: ['chromium', 'firefox', 'webkit'],
+      exitOnPageError: false, // GitHub currently throws errors
+      launchOptions: {
+        headless: true
+      }
+    }
   },
   testMatch: process.env.VITE_TEST_BUILD
     ? ['**/playground/**/*.spec.[jt]s?(x)']
     : ['**/*.spec.[jt]s?(x)'],
   testTimeout: process.env.CI ? 30000 : 10000,
-  // globalSetup: './scripts/jestGlobalSetup.js',
-  // globalTeardown: './scripts/jestGlobalTeardown.js',
-  // testEnvironment: './scripts/jestEnv.js',
-  // setupFilesAfterEnv: ['./scripts/jestPerTestSetup.ts'],
-  // watchPathIgnorePatterns: ['<rootDir>/temp'],
   // moduleNameMapper: {
   //   testUtils: '<rootDir>/packages/playground/testUtils.ts'
   // },
   globals: {
     'ts-jest': {
-      tsconfig: './packages/playground/tsconfig.json'
+      tsconfig: './playground/tsconfig.json'
     }
   }
 }
