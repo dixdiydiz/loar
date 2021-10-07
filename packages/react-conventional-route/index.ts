ca42c9d376ca5bf0cbe3b1c660917d660b7f9a07
dry <dengry.diy@gmail.com>
2021-10-07 22:40:59 +0800

    feat: route design

--- packages/react-conventional-route/index.ts
+++ packages/react-conventional-route/index.ts
@@ -1,1 +1,34 @@
-export {}
+// @ts-ignore
+import { globbySync } from 'globby'
+
+export interface Route {
+  path: string
+  element: string
+  children?: Route[]
+}
+
+export default function (
+  root: string,
+  routeMatch?: string[],
+  exclude?: string[]
+) {
+  routeMatch = Array.isArray(routeMatch)
+    ? routeMatch
+    : typeof routeMatch === 'string'
+    ? [routeMatch]
+    : ['**/*.{js,jsx,ts,tsx}']
+  exclude = Array.isArray(exclude)
+    ? exclude
+    : typeof exclude === 'string'
+    ? [exclude]
+    : ['**/__tests__/**']
+  const routes: Route[] = []
+  const paths = globbySync(
+    [...routeMatch, ...exclude.map((str) => `!${str}`)],
+    {
+      cwd: root,
+      onlyFiles: false
+    }
+  )
+  return routes
+}
