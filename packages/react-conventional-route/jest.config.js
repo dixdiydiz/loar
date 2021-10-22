626cde0b0948fe98df7cce46df62fda391fee545
dry <dengry.diy@gmail.com>
2021-10-22 19:53:36 +0800

    feat: delete route

--- packages/react-conventional-route/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$',
-  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/']
-}
--- playground/features/src/index.js
+++ playground/features/src/index.js
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom'
 import Env from './importMetaEnv'
 import StyleText from './style/styleText'
 
-// const ReqCtx = require.context('./pages', false, /.*.js/)
-const ReqCtx = require.context(
-  '@pages',
-  true,
-  /\.\/((?<!__tests__).)*\.route\.js/
-)
-console.log(ReqCtx.keys())
-// console.log(ReqCtx.keys().filter((p) => /\.\//.test(p)))
-// const Home = ReqCtx('./pages/index.js').default
-
 ReactDOM.render(
   <React.StrictMode>
     <div>
       <p id="app">1</p>
       <Env />
       <StyleText />
-      {/*<Home />*/}
     </div>
   </React.StrictMode>,
   document.getElementById('root')
